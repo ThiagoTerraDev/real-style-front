0d5b31d39babc3107d579cdbfd0a997d0bfaf7c1
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-09-15 15:42:13 -0300

    Criação, estilização e habilitação do modal de edição

--- js/modules/botao-editar-ordens.js
+++ js/modules/botao-editar-ordens.js
@@ -8,4 +8,40 @@ export default function criarBotaoEditar(parent) {
     botaoEditar.className = 'botao-editar';
 
     parent.appendChild(botaoEditar);
+
+    botaoEditar.addEventListener('click', abrirModalEditar)
+};
+
+
+// Função para abrir o modal de edição do produto da respectiva linha.
+
+function abrirModalEditar() {
+    const modalEditar = document.getElementsByClassName('modal-container')[0];
+    modalEditar.classList.add('active');
+
+    const linhaRespectiva = this.parentElement.parentElement;
+    const dataIndex = linhaRespectiva.getAttribute('data-index');
+    const botaoSalvar = document.getElementById('botao-salvar-modal');
+    botaoSalvar.setAttribute('data-index', dataIndex);
+
+    const quantidade = linhaRespectiva.getElementsByTagName('td')[1].innerHTML;
+    const novaQuantidade = document.getElementById('nova-quantidade');
+    novaQuantidade.value = quantidade;
+
+    const nomeProduto = linhaRespectiva.getElementsByTagName('td')[0].innerText;    
+    const nomeProdutoModal = document.getElementsByClassName('nome-produto-modal')[0];    
+    nomeProdutoModal.innerText = nomeProduto;
+};
+
+
+// Função para fechar o modal de edição de acordo com o clique no
+// botão "Close"
+
+export function fecharModalEditar() {
+    const botaoClose = document.getElementsByClassName('fechar-modal');
+    const modalEditar = document.getElementsByClassName('modal-container')[0];
+
+    botaoClose[0].onclick = () => {
+        modalEditar.classList.remove('active');
+    };
 };
--- js/scripts-ordens.js
+++ js/scripts-ordens.js
@@ -1,7 +1,10 @@
+import { fecharModalEditar } from "./modules/botao-editar-ordens.js";
 import buscarProdutos from "./modules/buscar-produtos-base.js";
 import definirTermoBusca from "./modules/formulario-busca-produto.js";
+
 
 document.addEventListener('DOMContentLoaded', () => {
     buscarProdutos();
     definirTermoBusca();    
+    fecharModalEditar();
 });
