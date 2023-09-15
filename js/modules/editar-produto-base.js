c55afda3ab7107269dae1435156a4d2113ee4dde
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-09-15 20:13:53 -0300

    Criação de função para editar quantidade de um produto da base

--- js/modules/editar-produto-base.js
+++ js/modules/editar-produto-base.js
@@ -10,5 +10,32 @@ export function salvarNovaQuantidade() {
 
         const novaQuantidade = document.getElementById('nova-quantidade');
         linhaRespectiva.getElementsByTagName('td')[1].innerText = novaQuantidade.value;
+
+        const nomeProduto = linhaRespectiva.getElementsByTagName('td')[0].innerText
+
+        editarProdutoBase(nomeProduto, novaQuantidade.value);
+
+        alert('New quantity successfully defined!')
     };
 }; 
+
+
+// Função que possibilita o envio de requisição de edição para o servidor, 
+// através do clique no botão "Save changes".
+
+function editarProdutoBase(nomeProduto, novaQuantidade) { 
+    const formData = new FormData();
+    formData.append('nome', nomeProduto);
+    formData.append('quantidade', novaQuantidade);
+
+    const url = 'http://127.0.0.1:5000/editar_produto';
+
+    return fetch(url, {
+        method: 'put',
+        body: formData
+    })
+        .then((response) => response.json())
+        .catch((error) => {
+            console.error('Error', error)
+        });
+};
