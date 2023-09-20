9c66fc6375fb68dbc5b3ca878cfa69c884b87f75
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-09-20 11:16:46 -0300

    Implementação de overlay para destacar a sidebar do carrinho de compras

--- js/modules/carrinho-sidebar.js
+++ js/modules/carrinho-sidebar.js
@@ -1,16 +1,19 @@
 const carrinhoSidebar = document.querySelector('.carrinho-sidebar');
 const carrinhoCompras = document.querySelector('.carrinho-compras');
 const fecharCarrinho = document.querySelector('#fechar-carrinho');
+const overlay = document.getElementById('overlay');
 
 
 export function abrirCarrinho() {
     carrinhoSidebar.onclick = () => {
         carrinhoCompras.classList.add('active');
+        overlay.style.display = 'block';
     };
 };
 
 export function ocultarCarrinho() {
     fecharCarrinho.onclick = () => {
         carrinhoCompras.classList.remove('active');
+        overlay.style.display = 'none';
     };
 };
